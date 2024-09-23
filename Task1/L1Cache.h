6de1733127af63dd427ed6e7f53d2a12891e4059
Tiago Santos <tiago.gouveia.santos@tecnico.ulisboa.pt>
2024-09-23 13:02:27 +0100

    Task1 feita

---
+++ Task1/L1Cache.h
@@ -0,0 +1,45 @@
+#ifndef L1CACHE_H
+#define L1CACHE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "../Cache.h"
+
+/*************** Auxiliar *********************/
+uint32_t log2(uint32_t x);
+
+uint32_t pow(uint32_t x, uint32_t e);
+
+/**************** Time Manipulation ***************/
+void resetTime();
+
+uint32_t getTime();
+
+/****************  RAM memory (byte addressable) ***************/
+void accessDRAM(uint32_t, uint8_t *, uint32_t);
+
+/*********************** Cache *************************/
+
+void initCache();
+void accessL1(uint32_t, uint8_t *, uint32_t);
+
+typedef struct CacheLine {
+  uint8_t Valid;
+  uint8_t Dirty;
+  uint32_t Tag;
+} CacheLine;
+
+typedef struct CacheL1 {
+  uint32_t init;
+  CacheLine lines[L1_SIZE/BLOCK_SIZE];
+} CacheL1;
+
+/*********************** Interfaces *************************/
+
+void read(uint32_t, uint8_t *);
+
+void write(uint32_t, uint8_t *);
+
+#endif
